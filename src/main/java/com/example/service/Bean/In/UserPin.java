a2b6465ab734c80a2c5c9259212e3c2a7ee61cda
lycyy <lyc945009953@gmail.com>
2022-09-19 13:06:12 +0800

    v6.1-逻辑更改，数据加密

---
+++ src/main/java/com/example/service/Bean/In/UserPin.java
@@ -0,0 +1,24 @@
+package com.example.service.Bean.In;
+
+public class UserPayPswd {
+    private String oldPin;
+    private String newPin;
+
+    public String getOldPin() {
+        return oldPin;
+    }
+
+    public void setOldPin(String oldPin) {
+        this.oldPin = oldPin;
+    }
+
+    public String getNewPin() {
+        return newPin;
+    }
+
+    public void setNewPin(String newPin) {
+        this.newPin = newPin;
+    }
+
+
+}
---
+++ src/main/java/com/example/service/Bean/In/Verify.java
@@ -0,0 +1,2 @@
+package com.example.service.Bean.In;public class Verify {
+}
